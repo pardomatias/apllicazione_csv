4d10a7124404c51eb3f8566130af269ed31426d9
pardomatias <pardoserrudo.matiasnahuel.studente@itispaleocapa.it>
2026-05-15 11:22:08 +0200

    carica_dati_visualizza_applicazione.cpp

--- applicazione.cpp
+++ applicazione.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <sstream>
 using namespace std;
 
 struct location
@@ -13,7 +14,7 @@ struct numerazionecivica
 {
 	string classetoponimo;
 	string tipotoponimo;
-	int numero;
+	string numero;
 	string subalterno;
 	string CAP;
 	string sezioneistat;
@@ -22,8 +23,36 @@ struct numerazionecivica
 	location locuzione;
 };
 
-void aggiungi()
-/*void lettura_strut(int x)
+void aggiungi_dati(int i, numerazionecivica dati[])
+{
+    ifstream fileInput("Comune_Bergamo_-_Numerazione_civica.csv");
+    string linea;
+
+    if (!fileInput.is_open())
+    {
+        cout << "Impossibile aprire il file\n";
+        return;
+    }
+
+    getline(fileInput, linea);
+
+    while (i < 1000 && getline(fileInput, linea))
+    {
+        stringstream riga(linea);
+
+        getline(riga, dati[i].classetoponimo, ',');
+        getline(riga, dati[i].tipotoponimo, ',');
+        getline(riga, dati[i].numero, ',');
+        getline(riga, dati[i].subalterno, ',');
+        getline(riga, dati[i].CAP, ',');
+        getline(riga, dati[i].sezioneistat, ',');
+
+        i++;
+    }
+
+    fileInput.close();
+    }
+void stampa_file(int x)
 {
 	ifstream fileInput("Comune_Bergamo_-_Numerazione_civica.csv");
     string linea;
@@ -36,13 +65,26 @@ void aggiungi()
     } else {
         cout << "Impossibile aprire il file";
     }
-}*/
+}
+string visualizza(numerazionecivica ele[], int dim){
+	string s;
+	for(int i=0;i<dim;i++){
+		s+=ele[i].classetoponimo+"\t";
+		s+=ele[i].tipotoponimo+"\t";
+		s+=ele[i].numero+"\t";
+		s+=ele[i].subalterno+"\t";
+		s+=ele[i].CAP+"\t";
+		s+=ele[i].sezioneistat+"\n";
+		
+	}
+	return s;
+	}
 
 
 int main() {
-	numerazionecivica dati[100];
+	numerazionecivica dati[1000];
 	int f=0;
-	/*do
+	do
 	{
 		cout<<"\n";
 		cout<<"\n";
@@ -52,23 +94,19 @@ int main() {
 		cin>>f;
 		cout<<"\n";
 		cout<<"\n";
-		fflush(stdin);*/
-	//lettura_strut(100);
-		ifstream fileInput("Comune_Bergamo_-_Numerazione_civica.csv");
-    string linea;
-
-    if (fileInput.is_open()) 
-	{
-        while (fileInput>>linea)
-		{ 
-            cout << linea << '\n';
-        }
-        fileInput.close();
-    } 
-	else 
-	{
-        cout << "Impossibile aprire il file";
-    }
+		fflush(stdin);
+		
+		switch(f)
+		{
+			case 1:
+				aggiungi_dati(1000,dati);
+				break;
+				
+			case 2:
+				visualizza(dati,1000);
+				break;	
+		}
+	}while(f!=0);	
 
     return 0;
 }
